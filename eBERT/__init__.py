6c45041ec3ecbbf0e947e7b8daa7d59d1982ae29
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2020-08-02 16:06:30 +0100

    Update __init__.py

--- eBERT/__init__.py
+++ eBERT/__init__.py
@@ -1,15 +1,23 @@
 from ._version import __version__  
 
-from .bert import *
-from .albert import *
-from eBERT import pretraining
-from .pretraining import *
-from eBERT import finetune
-from .finetune import *
-from eBERT import train
-from .train import *
-from eBERT import processing
-from .processing import *
+from . import bert
+from . import albert
+from . import tokenize
+from . import pretraining
+from . import finetune
+from . import train
+from . import processing
+
+#from .bert import *
+#from .albert import *
+#from eBERT import pretraining
+#from .pretraining import *
+#from eBERT import finetune
+#from .finetune import *
+#from eBERT import train
+#from .train import *
+#from eBERT import processing
+#from .processing import *
 #from eBERT.bert import BertConfig, Bert, Encoder, MultiHeadAttention
 #from eBERT.albert import AlbertConfig, Albert, EncoderGroup, Encoder, MultiHeadAttention
 #from eBERT.finetune import BertClassification
